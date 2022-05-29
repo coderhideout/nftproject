f7100fbd6d1718b53306ffdf74b6012678c3d5b6
Ricardo <RGoncalves95@hotmail.com>
2022-05-29 22:48:04 +0100

    fix deploy png ?

--- api/hello/index.ts
+++ api/hello/index.ts
@@ -8,7 +8,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
         //string htmlFilePath = Path.Combine(context.FunctionAppDirectory, "Data", "test.html");
-        var fileImage = context.executionContext.functionDirectory + "\\data\\test.png";
+        var fileImage = context.executionContext.functionDirectory + "/data/test.png";
         var data = getSync(fileImage);
     context.res = {
         // status: 200, /* Defaults to 200 */
