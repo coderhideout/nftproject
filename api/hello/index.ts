8e69422c6f4229ff4521827788873b34a43a5c54
Ricardo <RGoncalves95@hotmail.com>
2022-05-29 22:28:06 +0100

    test deploy imagem

--- api/hello/index.ts
+++ api/hello/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { get, getSync } from '@andreekeberg/imagedata'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -6,11 +7,16 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+        //string htmlFilePath = Path.Combine(context.FunctionAppDirectory, "Data", "test.html");
+        var fileImage = context.executionContext.functionDirectory + "\\data\\test.png";
+        var data = getSync(fileImage);
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
             input: name,
+            dir : context.executionContext.functionDirectory,
+            file: fileImage,
+            image: data,
             message: responseMessage
         }
     };
